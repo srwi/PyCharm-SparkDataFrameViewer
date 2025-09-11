7d7e9e262e0872dd7b2c8bbd45a215e2347d2e44
Stephan Rumswinkel <17520641+srwi@users.noreply.github.com>
2025-09-11 16:37:34 +0200

    Add icons

--- src/main/kotlin/com/github/srwi/sparkdataframeviewer/icons/DataFrameViewerIcons.kt
+++ src/main/kotlin/com/github/srwi/sparkdataframeviewer/icons/DataFrameViewerIcons.kt
@@ -5,4 +5,6 @@ import com.intellij.openapi.util.IconLoader
 object DataFrameViewerIcons {
     @JvmField
     val Heart = IconLoader.getIcon("/icons/heart.svg", javaClass)
+    @JvmField
+    val ToolWindow = IconLoader.getIcon("/icons/tool_window.svg", javaClass)
 }
